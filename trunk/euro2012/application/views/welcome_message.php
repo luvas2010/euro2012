ba9c52b58755f6befa01e1fa8a92e6c18fe949f4
john.schop@gmail.com <john.schop@gmail.com@015f8b00-9ce4-aea8-41a2-04a423ac0c9f>
2011-02-03 04:36:24 +0000

    there were some validation errors

--- trunk/euro2012/application/views/welcome_message.php
+++ trunk/euro2012/application/views/welcome_message.php
@@ -3,7 +3,6 @@
 $this->lang->load('welcome', language() );
 ?>
 
-<p><?php echo $this->lang->line('intro');?> <?php echo $settings['poolname']; ?>
-<p>
+<p><?php echo $this->lang->line('intro');?> <?php echo $settings['poolname']; ?></p>
 <p><?php echo anchor('login',$this->lang->line('login'));?></p>
-<p><?php echo anchor('signup',$this->lang->line('create'));?>
+<p><?php echo anchor('signup',$this->lang->line('create'));?></p>
