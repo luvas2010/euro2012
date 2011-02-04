847cb5b3409048d37771ed0d9a58dfd54d515b04
john.schop@gmail.com <john.schop@gmail.com@015f8b00-9ce4-aea8-41a2-04a423ac0c9f>
2011-02-04 01:44:45 +0000

    to hold larger peices of text

---
+++ trunk/euro2012/application/models/texts.php
@@ -0,0 +1,16 @@
+<?php
+
+class Texts extends Doctrine_Record {
+    
+   public function setTableDefinition() {
+        $this->hasColumn('text_name', 'string', 255);
+        $this->hasColumn('text_en', 'clob', array('notnull' => false));
+        $this->hasColumn('text_nl', 'clob', array('notnull' => false));
+        $this->hasColumn('default', 'clob', array('notnull' => false));            
+    }
+    
+    public function setUp() {
+
+    }
+    
+}
