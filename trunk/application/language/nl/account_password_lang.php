0a90032ef4e805d57abbde47416c6cd3140d4734
doncartel@gmail.com <doncartel@gmail.com@015f8b00-9ce4-aea8-41a2-04a423ac0c9f>
2012-01-10 14:25:44 +0000

    vergetem-> vergeten ninimaal-> minimaal en paar punten weg gehaald ivm consistentie

--- trunk/application/language/nl/account_password_lang.php
+++ trunk/application/language/nl/account_password_lang.php
@@ -10,12 +10,12 @@ $lang['password_current_password']				= 'Huidig wachtwoord';
 $lang['password_new_password']					= 'Nieuw wachtwoord';
 $lang['password_retype_new_password']			= 'Nieuw wachtwoord bevestigen';
 $lang['password_change_my_password']			= 'Wijzig mijn wachtwoord';
-$lang['password_current_password_incorrect']	= 'Je huidige wachtwoord is incorrect.';
-$lang['password_forgot_your_password']			= 'Wachtwoord vergetem?';
-$lang['password_password_has_been_changed']		= 'Je wachtwoord is gewijzigd.';
+$lang['password_current_password_incorrect']	= 'Je huidige wachtwoord is incorrect';
+$lang['password_forgot_your_password']			= 'Wachtwoord vergeten?';
+$lang['password_password_has_been_changed']		= 'Je wachtwoord is gewijzigd';
 $lang['password_safe_guard_your_account']		= '<div class="info"><p>Voor de veiligheid van je account wordt geadviseerd om een complex wachtwoord te kiezen met de volgende eigenschappen:</p>
                                                     <ul class="list">
-                                                        <li>een lengte van ninimaal 8 tekens</li>
+                                                        <li>een lengte van minimaal 8 tekens</li>
                                                         <li>bevat zowel hoofd- als kleine letters</li>
                                                         <li>bevat getallen, speciale tekens en leestekens</li>
                                                         <li>is niet gebaseerd op persoonlijke informatie</li>
