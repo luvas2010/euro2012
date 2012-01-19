99727adf50521436a07e97b1b2b9c144e255c51f
john.schop <john.schop@015f8b00-9ce4-aea8-41a2-04a423ac0c9f>
2012-01-19 14:49:02 +0000

    Need the language files for shouts to work...

---
+++ trunk/application/language/en/shouts_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['shout_header']           = "Posted by %s on %s at %s";
+$lang['all_shouts']             = "All messages";
---
+++ trunk/application/language/nl/shouts_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['shout_header']           = "Geplaatst door %s op %s om %s uur";
+$lang['all_shouts']             = "Alle berichten";
