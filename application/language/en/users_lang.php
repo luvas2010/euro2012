f1661bb8b59b7ba8799e010bfe6801075a044349
john.schop@gmail.com <john.schop@gmail.com@015f8b00-9ce4-aea8-41a2-04a423ac0c9f>
2012-02-05 04:21:50 +0000

    users foutje

--- application/language/en/users_lang.php
+++ application/language/en/users_lang.php
@@ -1,6 +1,6 @@
 <?php
 
 $lang['username']       = "User Name";
-
+$lang['points_total']	= "Points";
 /* End of file standings_lang.php */
 /* Location: ./application/language/english/standings_lang.php */
