5f2567c65d314ee69e662eacdc9fc405e269952f
john.schop <john.schop@015f8b00-9ce4-aea8-41a2-04a423ac0c9f>
2012-02-28 18:57:09 +0000

    charts now only available if logged in.

--- trunk/application/controllers/charts.php
+++ trunk/application/controllers/charts.php
@@ -24,129 +24,148 @@ class Charts extends CI_Controller {
     
     function top($num = 10)
     {
-        $this->load->library('pool');
-        $topusers = $this->pool->get_top_ranking($num);
-        $data = array(
-                        'topusers'          => $topusers,
-                        'num'               => $num,
-                        'account'           => $this->account_model->get_by_id($this->session->userdata('account_id')),
-                        'account_details'   => $this->account_details_model->get_by_account_id($this->session->userdata('account_id'))
-                        );
-            $data['content_main'] = "charts_topuser";
-            $data['title'] = lang('standings');
-    
-            $this->load->view('template/template', $data);
+        if ($this->authentication->is_signed_in())
+        {
+            $this->load->library('pool');
+            $topusers = $this->pool->get_top_ranking($num);
+            $data = array(
+                            'topusers'          => $topusers,
+                            'num'               => $num,
+                            'account'           => $this->account_model->get_by_id($this->session->userdata('account_id')),
+                            'account_details'   => $this->account_details_model->get_by_account_id($this->session->userdata('account_id'))
+                            );
+                $data['content_main'] = "charts_topuser";
+                $data['title'] = lang('standings');
         
+                $this->load->view('template/template', $data);
+        }
+        else
+        {
+            redirect('account/sign_in/?continue='.site_url('charts/top/'.$num));
+        }
     }
     
     function champion()
     {
-        
-        $sql_query = "SELECT `pred_champion`, COUNT(`account_id`) as `number`
-                      FROM `account_details`
-                      WHERE `pred_champion` <> ''
-                      AND   `pred_champion` <> 'NULL'
-                      GROUP BY `pred_champion`";
-        $query = $this->db->query($sql_query);
-        $champions = $query->result_array();
-        $total = 0;
-        if ($query->num_rows() > 0)
+        if ($this->authentication->is_signed_in())
         {
-            foreach ($champions as $champion)
+            $sql_query = "SELECT `pred_champion`, COUNT(`account_id`) as `number`
+                          FROM `account_details`
+                          WHERE `pred_champion` <> ''
+                          AND   `pred_champion` <> 'NULL'
+                          GROUP BY `pred_champion`";
+            $query = $this->db->query($sql_query);
+            $champions = $query->result_array();
+            $total = 0;
+            if ($query->num_rows() > 0)
             {
-                $total = $total + $champion['number'];
-            }
-            reset($champions);
-            foreach ($champions as $c)
-            {
-                $percentage[$c['pred_champion']] = number_format(($c['number'] / $total) * 100, 1);
-                
-            }
-            //echo "<pre>"; print_r($percentage); echo "</pre>";
-            //echo "<pre>"; print_r($champions); echo "</pre>";
-            $data = array(
-                            'percentage'        => $percentage,
-                            'account'           => $this->account_model->get_by_id($this->session->userdata('account_id')),
-                            'account_details'   => $this->account_details_model->get_by_account_id($this->session->userdata('account_id'))
-                            );
-         }
-         else
-         {
-            $data = array(
-                            'percentage'        => 0,
-                            'account'           => $this->account_model->get_by_id($this->session->userdata('account_id')),
-                            'account_details'   => $this->account_details_model->get_by_account_id($this->session->userdata('account_id'))
-                            );
-        }                  
-            $data['content_main'] = "charts_champion";
-            $data['title'] = lang('standings');
-    
-            $this->load->view('template/template', $data);
+                foreach ($champions as $champion)
+                {
+                    $total = $total + $champion['number'];
+                }
+                reset($champions);
+                foreach ($champions as $c)
+                {
+                    $percentage[$c['pred_champion']] = number_format(($c['number'] / $total) * 100, 1);
+                    
+                }
+                //echo "<pre>"; print_r($percentage); echo "</pre>";
+                //echo "<pre>"; print_r($champions); echo "</pre>";
+                $data = array(
+                                'percentage'        => $percentage,
+                                'account'           => $this->account_model->get_by_id($this->session->userdata('account_id')),
+                                'account_details'   => $this->account_details_model->get_by_account_id($this->session->userdata('account_id'))
+                                );
+             }
+             else
+             {
+                $data = array(
+                                'percentage'        => 0,
+                                'account'           => $this->account_model->get_by_id($this->session->userdata('account_id')),
+                                'account_details'   => $this->account_details_model->get_by_account_id($this->session->userdata('account_id'))
+                                );
+            }                  
+                $data['content_main'] = "charts_champion";
+                $data['title'] = lang('standings');
+        
+                $this->load->view('template/template', $data);
+        }
+        else
+        {
+            redirect('account/sign_in/?continue='.site_url('charts/champion'));
+        }
     }
 
     function totalgoals()
     {
         
-        $sql_query = "SELECT `pred_total_goals`, COUNT(`account_id`) as `number`
-                      FROM `account_details`
-                      WHERE `pred_total_goals` > 0
-                      AND   `pred_champion` <> 'NULL'
-                      GROUP BY `pred_total_goals`";
-        $query = $this->db->query($sql_query);
-        $totalgoals = $query->result_array();
-        if ($query->num_rows() > 0)
-        { 
-            foreach ($totalgoals as $total)
-            {
-                $pred_goals[$total['pred_total_goals']] = $total['number'];
-            }    
-            
-            
-            $sql_query = "SELECT *
-                          FROM `match`
-                          WHERE `match_calculated` = 1";
+        if ($this->authentication->is_signed_in())
+        {
+            $sql_query = "SELECT `pred_total_goals`, COUNT(`account_id`) as `number`
+                          FROM `account_details`
+                          WHERE `pred_total_goals` > 0
+                          AND   `pred_champion` <> 'NULL'
+                          GROUP BY `pred_total_goals`";
             $query = $this->db->query($sql_query);
-            $calculated_matches = $query->result_array();
-            $num_calculated_matches = $query->num_rows();
-            $goals_scored = 0;
-            foreach ($calculated_matches as $match)
-            {
-                $goals_scored = $goals_scored + $match['home_goals'] + $match['away_goals'];
-            }
-            if ($num_calculated_matches > 0)
-            {
-                $average_goals = $goals_scored/$num_calculated_matches;
+            $totalgoals = $query->result_array();
+            if ($query->num_rows() > 0)
+            { 
+                foreach ($totalgoals as $total)
+                {
+                    $pred_goals[$total['pred_total_goals']] = $total['number'];
+                }    
+                
+                
+                $sql_query = "SELECT *
+                              FROM `match`
+                              WHERE `match_calculated` = 1";
+                $query = $this->db->query($sql_query);
+                $calculated_matches = $query->result_array();
+                $num_calculated_matches = $query->num_rows();
+                $goals_scored = 0;
+                foreach ($calculated_matches as $match)
+                {
+                    $goals_scored = $goals_scored + $match['home_goals'] + $match['away_goals'];
+                }
+                if ($num_calculated_matches > 0)
+                {
+                    $average_goals = $goals_scored/$num_calculated_matches;
+                }
+                else
+                {
+                    $average_goals = 0;
+                }    
+                $total_with_average = $average_goals * 31;
+                
+                $data = array(
+                                'pred_goals'        => $pred_goals,
+                                'average_goals'     => $average_goals,
+                                'num_matches'       => $num_calculated_matches,
+                                'total_with_average' => $total_with_average,
+                                'account'           => $this->account_model->get_by_id($this->session->userdata('account_id')),
+                                'account_details'   => $this->account_details_model->get_by_account_id($this->session->userdata('account_id'))
+                                );
             }
             else
             {
-                $average_goals = 0;
-            }    
-            $total_with_average = $average_goals * 31;
-            
-            $data = array(
-                            'pred_goals'        => $pred_goals,
-                            'average_goals'     => $average_goals,
-                            'num_matches'       => $num_calculated_matches,
-                            'total_with_average' => $total_with_average,
-                            'account'           => $this->account_model->get_by_id($this->session->userdata('account_id')),
-                            'account_details'   => $this->account_details_model->get_by_account_id($this->session->userdata('account_id'))
-                            );
+                $data = array(
+                    'pred_goals'        => 0,
+                    'average_goals'     => 0,
+                    'num_matches'       => 0,
+                    'total_with_average' => 0,
+                    'account'           => $this->account_model->get_by_id($this->session->userdata('account_id')),
+                    'account_details'   => $this->account_details_model->get_by_account_id($this->session->userdata('account_id'))
+                    );
+            }
+                $data['content_main'] = "charts_total_goals";
+                $data['title'] = lang('standings');
+        
+                $this->load->view('template/template', $data);
         }
         else
         {
-            $data = array(
-                'pred_goals'        => 0,
-                'average_goals'     => 0,
-                'num_matches'       => 0,
-                'total_with_average' => 0,
-                'account'           => $this->account_model->get_by_id($this->session->userdata('account_id')),
-                'account_details'   => $this->account_details_model->get_by_account_id($this->session->userdata('account_id'))
-                );
+            redirect('account/sign_in/?continue='.site_url('totalgoals'));
         }
-            $data['content_main'] = "charts_total_goals";
-            $data['title'] = lang('standings');
-    
-            $this->load->view('template/template', $data);
     }
     
 }
