58201e4da3701cea446a4942078eec205b8e0b84
john.schop <john.schop@015f8b00-9ce4-aea8-41a2-04a423ac0c9f>
2012-03-09 15:59:09 +0000

    forgot there is &szlig; in German. (Ringel s)

--- trunk/application/language/de/poolconfig_lang.php
+++ trunk/application/language/de/poolconfig_lang.php
@@ -3,7 +3,7 @@ $lang['version']                    = "Pool Software Version";
 $lang['pool_name']                  = "Name Ihres Pools";
 $lang['time_offset']                = "Zeitausgleich des CET (Central European Time) auf dem gastgebenen Server , in Sekunden";
 $lang['predictions_open']           = "Wenn ausgelegt bei 1, k&ouml;nnen die Nutzer das Spiel bis zum Anpfiff des Spiels tippen. Wenn ausgelegt bei 0, k&ouml;nnen die Nutzer das Spielergebnis bis zum Spielstart tippen.";
-$lang['predictions_open_offset']    = "Die H&ouml;he der Sekundentipps, schlieﬂen sich, bevor das Spiel beginnt.Steht 'Tippen_offen' bei 0, schlieﬂt sich die H&ouml;he der Sekundentipps bevor das Spiel #1 beginnt. ";
+$lang['predictions_open_offset']    = "Die H&ouml;he der Sekundentipps, schlie&szlig;en sich, bevor das Spiel beginnt.Steht 'Tippen_offen' bei 0, schlie&szlig;t sich die H&ouml;he der Sekundentipps bevor das Spiel #1 beginnt. ";
 $lang['public_predictions']         = "Wenn ausgegelegt bei 1, k&ouml;nnen die Nutzer die Tipps der anderen Teilnehmer sehen.";
 $lang['public_social_links']        = "Wenn ausgelegt auf 1, k&ouml;nnen die Nutzer die verbundenen Sozialennetzwerkaccounts sehen.";
 
