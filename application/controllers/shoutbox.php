f09822a185d8b909df6e65e547a8a68732be4e44
john.schop <john.schop@015f8b00-9ce4-aea8-41a2-04a423ac0c9f>
2012-04-06 14:41:52 +0000

    Extra check for when user has no language selected

--- application/controllers/shoutbox.php
+++ application/controllers/shoutbox.php
@@ -14,7 +14,8 @@ class Shoutbox extends CI_Controller {
         $this->db->select('language');
         $query = $this->db->get_where('account_details', array('account_id' => $this->session->userdata('account_id')));
         $lang = $query->row_array();
-        if (isset($lang['language'])) $this->config->set_item('language',$lang['language']);        
+        if ($lang['language'] == "") { $lang['language'] = $this->config->item('language'); }
+        if (isset($lang['language'])) $this->config->set_item('language',$lang['language']);       
         $this->lang->load(array('general', 'welcome', 'shouts'));
     }
     
@@ -33,6 +34,7 @@ class Shoutbox extends CI_Controller {
 		$this->db->select('language');
         $query = $this->db->get_where('account_details', array('account_id' => $this->session->userdata('account_id')));
         $lang = $query->row_array();
+        if ($lang['language'] == "") { $lang['language'] = $this->config->item('language'); }
         if (isset($lang['language'])) $this->config->set_item('language',$lang['language']);        
         $this->lang->load(array('general', 'welcome', 'shouts'));
         $shout = addslashes(strip_tags(trim($this->input->post('shouttxt'))));
